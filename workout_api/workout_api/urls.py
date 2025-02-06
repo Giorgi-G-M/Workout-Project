98588018a76dfed04cbacf7a9d6c55ff04c60c7b
Giorgi-G-M <mzhavanadze1999@gmail.com>
2025-02-07 02:39:13 +0400

    add JWT authentication with simplejwt

--- workout_api/workout_api/urls.py
+++ workout_api/workout_api/urls.py
@@ -20,6 +20,7 @@ from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema =  get_schema_view(openapi.Info(title='Workout API',default_version='v1',
                                        description='API documentatation for the Personalized Workout Plan System'),
@@ -28,5 +29,7 @@ schema =  get_schema_view(openapi.Info(title='Workout API',default_version='v1',
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("workouts.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/',schema.with_ui('swagger',cache_timeout=0),name='shema-swagger-ui'),
 ]
