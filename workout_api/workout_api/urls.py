397633ec13616e989fb6ec01ff71c2ae84f651e8
Giorgi-G-M <mzhavanadze1999@gmail.com>
2025-02-07 02:22:21 +0400

    add URL routing for core models

--- workout_api/workout_api/urls.py
+++ workout_api/workout_api/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema =  get_schema_view(openapi.Info(title='Workout API',default_version='v1',
+                                       description='API documentatation for the Personalized Workout Plan System'),
+                                       public=True, permission_classes=[permissions.AllowAny],)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/workouts/', include("workouts.urls"))
+    path('api/', include("workouts.urls")),
+    path('swagger/',schema.with_ui('swagger',cache_timeout=0),name='shema-swagger-ui'),
 ]
--- workout_api/workouts/urls.py
+++ workout_api/workouts/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ExerciseViewSet, WorkoutPlanViewSet, TrackingViewSet
+
+
+router = DefaultRouter()
+router.register(r'exercises', ExerciseViewSet)
+router.register(r'workout-plans', WorkoutPlanViewSet)
+router.register(r'tracking', TrackingViewSet)
+
 
 urlpatterns = [
-
-
-
-
+    path('', include(router.urls))
 ]
